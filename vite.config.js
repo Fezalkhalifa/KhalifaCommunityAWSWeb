b7fb5bd2548ce60635c78dd78980ce7d5085de34
Fezalkhalifa <63090868+Fezalkhalifa@users.noreply.github.com>
2022-12-05 20:45:10 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
+ /* build: {
     rollupOptions: {
       external: [
         "assets/css/styles.css",
         "assets/css/all.css"
       ]
     }
-  },
+  },*/
   plugins: [vue()],
   resolve: {
     alias: {
